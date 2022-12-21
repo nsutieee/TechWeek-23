aff654b4be09a050e6a6d5744f25ee933bbab3e8
AgastyaJXD7 <agastyaj.87075@gmail.com>
2022-12-21 23:23:42 +0530

    Variables Initialised

---
+++ src/components/NavBar.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+function NavBar() {
+  return (
+    <>
+        <div className="Navigation_Bar">
+        <div className="NavBar_Box">
+            <p>HOME</p>
+        </div>
+        <div className="NavBar_Box">
+            <p>ABOUT</p>
+        </div>
+        <div className="NavBar_Box">
+            <p>EVENTS</p>
+        </div>
+        <div className="NavBar_Box">
+            <p>GALLERY</p>
+        </div>
+        <div className="NavBar_Box">
+            <p>FAQs</p>
+        </div>
+    </div>
+    </>
+  )
+}
+
+export default NavBar
