ade99b816722a56e97722b8f4bc1204d4817665d
AgastyaJXD7 <agastyaj.87075@gmail.com>
2022-12-22 00:14:14 +0530

    More Variables Initialized , Minor Navbar Changes

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,26 +1,34 @@
 import React from 'react'
+import tcwklgo from '../images/TechWeekLogo.png'
 
 function NavBar() {
   return (
-    <>
-        <div className="Navigation_Bar">
-        <div className="NavBar_Box">
-            <p>HOME</p>
-        </div>
-        <div className="NavBar_Box">
-            <p>ABOUT</p>
-        </div>
-        <div className="NavBar_Box">
-            <p>EVENTS</p>
-        </div>
-        <div className="NavBar_Box">
-            <p>GALLERY</p>
-        </div>
-        <div className="NavBar_Box">
-            <p>FAQs</p>
-        </div>
-    </div>
-    </>
+      <>
+          <div className="Navigation_Bar">
+
+              <div className="Navigation_Bar_Left">
+                <img src={tcwklgo} alt="" />
+              </div>
+
+              <div className="Navigation_Bar_Right">
+                  <div className="NavBar_Box">
+                      <p>HOME</p>
+                  </div>
+                  <div className="NavBar_Box">
+                      <p>ABOUT</p>
+                  </div>
+                  <div className="NavBar_Box">
+                      <p>EVENTS</p>
+                  </div>
+                  <div className="NavBar_Box">
+                      <p>GALLERY</p>
+                  </div>
+                  <div className="NavBar_Box">
+                      <p>FAQs</p>
+                  </div>
+              </div>
+          </div>
+      </>
   )
 }
 
