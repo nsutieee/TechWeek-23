3a3ec82889f86b7c3f9abc94e6c84526dfb5a4b3
utkrsh1203 <utkrsh2212@gmail.com>
2022-12-23 20:10:25 +0530

    Events modified

--- src/components/EventCard/eventCard.js
+++ src/components/EventCard/eventCard.js
@@ -2,22 +2,33 @@ import React from "react";
 import "../../styles/Card.css";
 
 export default function EventCard(props) {
+  // let mouseOver = () => {
+  //   const card = document.querySelector("#" + props.id);
+  //   card.classList.add("is-flipped");
+  // };
+  // let mouseOut = () => {
+  //   const card = document.querySelector("#" + props.id);
+  //   card.classList.remove("is-flipped");
+  // };
+
   let click = () => {
     const card = document.querySelector("#" + props.id);
-    console.log(card);
     card.classList.toggle("is-flipped");
   };
 
   return (
-    <div className='card'>
-      <div className='card__inner' id={props.id} onClick={click}>
+    <div className='card' onClick={click}>
+      <div className='card__inner' id={props.id}>
         <div className='card__face card__face--front card-'>
           <h2>{props.frontHeader}</h2>
+          <div>
+            <button className='btn-14'>Register here</button>
+            <button className='btn-14'>More info</button>
+          </div>
         </div>
         <div className='card__face card__face--back'>
           <div className='card__content'>
             <div className='card__header'>
-              <img src='pp.jpg' alt='' className='pp' />
               <h2>{props.title}</h2>
             </div>
             <div className='card__body'>
--- src/components/EventCard/events.js
+++ src/components/EventCard/events.js
@@ -4,8 +4,8 @@ import "../../styles/events.css";
 
 export default function Events() {
   return (
-    <section className="events">
-    <div className='events'>
+    <section className='events'>
+      {/* <div className='events'> */}
       <EventCard
         id='card1'
         frontHeader='Event 1'
@@ -26,23 +26,23 @@ export default function Events() {
       />
       <EventCard
         id='card4'
-        frontHeader='Event 1'
-        title='Title 1'
+        frontHeader='Event 4'
+        title='Title 4'
         details='Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis, harum tenetur?'
       />
       <EventCard
         id='card5'
-        frontHeader='Event 2'
-        title='Title 2'
+        frontHeader='Event 5'
+        title='Title 5'
         details='Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis, harum tenetur? Dolor amet similique, ad culpa magnam alias eveniet odit labore quas deserunt consequun'
       />
       <EventCard
         id='card6'
-        frontHeader='Event 3'
-        title='Title 3'
+        frontHeader='Event 6'
+        title='Title 6'
         details='Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis, harum tenetur? Dolor amet similique,'
       />
-    </div>
+      {/* </div> */}
     </section>
   );
 }
