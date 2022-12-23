7c3ccf2b0fcba839ae79b857b658228015fbb5a2
AgastyaJXD7 <agastyaj.87075@gmail.com>
2022-12-23 12:43:31 +0530

    Footer & Header Fixes

--- src/components/FooterS/Footers.jsx
+++ src/components/FooterS/Footers.jsx
@@ -15,13 +15,13 @@ const Footers = () => {
             ></img>
             </div>
             </div>
-          <div>
+          <div className='Column_1_Des'>
           "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. 
           </div>
         </div>
         <div className="col brd">
-          <h3 className="footerhead">
-            Address{" "}
+          <h3 className="footer-header">
+            Address
          </h3>
       <div className="office-des">
               <section>
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,15 +7,7 @@ const Header = () => {
         <header>
         <div>
          <NavBar/>
-        </div>
-        <div className="headerItem">
-          <div className="headerItem1">
-                ITEM !
-          </div>
-          <div className="headerItem2">
-                Item 2
-          </div>
-        </div>
+        </div>       
         </header>
     )
 }
