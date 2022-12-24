d741137fd8fb8eb2cb8db71740103d9d3b91678b
utkrsh1203 <utkrsh2212@gmail.com>
2022-12-24 23:43:52 +0530

    About us responsiveness fixed

--- src/components/LandingPage/Home.js
+++ src/components/LandingPage/Home.js
@@ -1,12 +1,9 @@
-import React from 'react'
-import FlipperCountdown from '../FlipperCountdown'
-import './Home.css'
-import HeaderGraphic from './HeaderGraphic'
-import { Fade } from "react-reveal"
-import TechWeekLogoSplit from './techweekLogo'
-
-
-
+import React from "react";
+import FlipperCountdown from "../FlipperCountdown";
+import "./Home.css";
+import HeaderGraphic from "./HeaderGraphic";
+import { Fade } from "react-reveal";
+import TechWeekLogoSplit from "./techweekLogo";
 
 function Home() {
   return (
@@ -14,31 +11,39 @@ function Home() {
       <div className='Header_Ssn'>
         <div className='Master_Container'>
           <div className='Main_Header'>
-            <TechWeekLogoSplit/>
+            <TechWeekLogoSplit />
           </div>
-          <div className="Main_CountDown"> <FlipperCountdown /></div>
+          <div className='Main_CountDown'>
+            {" "}
+            <FlipperCountdown />
+          </div>
         </div>
         <div className='Graphic_Container'>
-        <Fade left>  <div className='Header_Graphic'><HeaderGraphic /></div></Fade>
-          <Fade right><div className='About_TchWk left' >
-            <h3>About Tech Week</h3>
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-              Cum minus voluptates expedita debitis voluptas! Quam accusamus,
-              aliquam itaque maiores quo nobis voluptatibus eius nihil quia
-              dolores dolor ipsam dignissimos corrupti,
-              obcaecati temporibus vero, accusantium quaerat.
-              Minus eum maxime earum quos quibusdam beatae velit ea ducimus,
-              mollitia quia illo esse cupiditate?
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-              Cum minus voluptates expedita debitis voluptas! Quam accusamus,
-             </p>
-          </div></Fade>
+          <Fade left>
+            {" "}
+            <div className='Header_Graphic'>
+              <HeaderGraphic />
+            </div>
+          </Fade>
+          <Fade right>
+            <div className='About_TchWk left'>
+              <h3>About Tech Week</h3>
+              <p>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cum
+                minus voluptates expedita debitis voluptas! Quam accusamus,
+                aliquam itaque maiores quo nobis voluptatibus eius nihil quia
+                dolores dolor ipsam dignissimos corrupti, obcaecati temporibus
+                vero, accusantium quaerat. Minus eum maxime earum quos quibusdam
+                beatae velit ea ducimus, mollitia quia illo esse cupiditate?
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cum
+                minus voluptates expedita debitis voluptas! Quam accusamus,
+              </p>
+            </div>
+          </Fade>
         </div>
-
-        
       </div>
     </section>
-  )
+  );
 }
 
-export default Home
+export default Home;
