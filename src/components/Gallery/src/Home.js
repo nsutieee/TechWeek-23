26837bdf8929b7b076ff9e47f98c687af5f66db2
Evenidk <119731862+Evenidk@users.noreply.github.com>
2022-12-24 15:55:59 +0530

    Add files via upload

---
+++ src/components/Gallery/src/Home.js
@@ -0,0 +1,59 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <React.Fragment>
+        <div class="heading">
+    <h1>TECHWEEK GALLERY</h1>
+</div>
+<ul class="honeycomb">
+    <li class="honeycomb-cell">
+        <img class="honeycomb-cell_img" src="assets/pikachu.png"/>
+        <div class="honeycomb-cell_title">Evenidk</div>
+    </li>
+    <li class="honeycomb-cell">
+        <img class="honeycomb-cell_img" src="assets/pikachu.png"/>
+        <div class="honeycomb-cell_title">Evenidk</div>
+    </li>
+    <li class="honeycomb-cell">
+        <img class="honeycomb-cell_img" src="assets/pikachu.png"/>
+        <div class="honeycomb-cell_title">Evenidk</div>
+    </li>
+    <li class="honeycomb-cell">
+        <img class="honeycomb-cell_img" src="assets/pikachu.png"/>
+        <div class="honeycomb-cell_title">Evenidk</div>
+    </li>
+    <li class="honeycomb-cell">
+        <img class="honeycomb-cell_img" src="assets/pikachu.png"/>
+        <div class="honeycomb-cell_title">Evenidk</div>
+    </li>
+    <li class="honeycomb-cell">
+        <img class="honeycomb-cell_img" src="assets/pikachu.png"/>
+        <div class="honeycomb-cell_title">Evenidk</div>
+    </li>
+    <li class="honeycomb-cell">
+        <img class="honeycomb-cell_img" src="assets/pikachu.png"/>
+        <div class="honeycomb-cell_title">Evenidk</div>
+    </li>
+    <li class="honeycomb-cell">
+        <img class="honeycomb-cell_img" src="assets/pikachu.png"/>
+        <div class="honeycomb-cell_title">Evenidk</div>
+    </li>
+    <li class="honeycomb-cell">
+        <img class="honeycomb-cell_img" src="assets/pikachu.png"/>
+        <div class="honeycomb-cell_title">Evenidk</div>
+    </li>
+    <li class="honeycomb-cell">
+        <img class="honeycomb-cell_img" src="assets/pikachu.png"/>
+        <div class="honeycomb-cell_title">Evenidk</div>
+    </li>
+    <li class="honeycomb-cell">
+        <img class="honeycomb-cell_img" src="assets/pikachu.png"/>
+        <div class="honeycomb-cell_title">Evenidk</div>
+    </li>
+    <li class="honeycomb-cell honeycomb_Hidden">
+    </li>
+</ul>
+    </React.Fragment>
+  )
+}
