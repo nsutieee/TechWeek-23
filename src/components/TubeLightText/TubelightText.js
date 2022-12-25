e71c6064959ffa610a5f0389c9edc24e4747aaaf
chaitanya-bhargava <chaitanyabhargava5@gmail.com>
2022-12-25 18:44:47 +0530

    added tubelight text component

---
+++ src/components/TubeLightText/TubelightText.js
@@ -0,0 +1,11 @@
+import './TubelightText.css';
+function TubelightText({text}) {
+    return (
+      <div className='tubelight'>
+        <p>{text}</p>
+      </div>
+    );
+  }
+  
+  export default TubelightText;
+  
