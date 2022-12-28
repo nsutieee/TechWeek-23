f7064fa568458fc2dd6f237c39d2c81a78f5bc43
utkrsh1203 <utkrsh2212@gmail.com>
2022-12-28 14:33:15 +0530

    Faq added on Homescreen

--- src/components/screens/HomeScreen.js
+++ src/components/screens/HomeScreen.js
@@ -1,33 +1,39 @@
-import React from 'react'
-import Events from '../EventCard/events'
+import React from "react";
+import Events from "../EventCard/events";
 //import Sponsors from '../Sponsors/Sponsors'
-import { Fade } from 'react-reveal'
-import Header from '../Header/Header'
-import Footers from '../FooterS/Footers'
-import Home from '../LandingPage/Home'
-import Timeline from '../timeline/timeline'
-import PastSponsor from '../pastSponsors/pastSponsors'
-import AnnouncingSponsors from '../Sponsors/AnnouncingSponsors'
+import { Fade } from "react-reveal";
+import Header from "../Header/Header";
+import Footers from "../FooterS/Footers";
+import Home from "../LandingPage/Home";
+import Timeline from "../timeline/timeline";
+import PastSponsor from "../pastSponsors/pastSponsors";
+import AnnouncingSponsors from "../Sponsors/AnnouncingSponsors";
 //import HamburgerMenu from '../Header/HamburgerMenu'
-import NewHeader from '../../NewHeaderFolder/NewHeader'
+import NewHeader from "../../NewHeaderFolder/NewHeader";
 //import Gallery from '../Gallery'
-
+import Faq from "../faq/faq";
 
 function HomeScreen() {
   return (
     <>
-    {/* <particleBackground/> */}
-    <Home/>
-    <NewHeader/>
-    <Fade up><Events/></Fade>
-    {/* <Gallery/> */}
-   
-    <Timeline/>
+      {/* <particleBackground/> */}
+      <Home />
+      <NewHeader />
+      <Fade up>
+        <Events />
+      </Fade>
+      {/* <Gallery/> */}
 
-    <Fade up><AnnouncingSponsors/></Fade>
-    <Fade up><PastSponsor/></Fade>   
-     
-    <Footers/>
+      <Timeline />
+
+      <Fade up>
+        <AnnouncingSponsors />
+      </Fade>
+      <Fade up>
+        <PastSponsor />
+      </Fade>
+      <Faq />
+      <Footers />
     </>
   );
 }
