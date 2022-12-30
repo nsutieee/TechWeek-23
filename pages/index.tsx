c6398d4f63ec0021d2f2783b703f6f45e4f7ed28
Ayush (hacktiger) <hacktiger.as@gmail.com>
2022-12-31 03:37:32 +0530

    fix: Sample bitch

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,5 +3,5 @@ import { Inter } from "@next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return <h1 className="text-3xl font-bold underline">Hello bitches!</h1>;
 }
